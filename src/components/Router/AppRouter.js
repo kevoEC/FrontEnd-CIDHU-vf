f1db6fb3574e542b0bd264803061b75cd87b294b
Kevo72 <115098318+kevoEC@users.noreply.github.com>
2024-02-05 00:02:16 -0500

    Hash

--- src/components/Router/AppRouter.js
+++ src/components/Router/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { PrivateRoute } from './PrivateRoute';
 import Login from '../Login/Login';
 import { Main } from '../Main/Main';
